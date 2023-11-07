cadc2ca57410f5292d085b3e6eb7e0ac2838ffcc
TNTSuperMan <tntsuperga@gmail.com>
2023-11-07 18:17:36 +0900

    v4

--- minify-php/index.php
+++ minify-php/index.php
@@ -1,4 +1,11 @@
 <?php
+function style(){
+    $style = "";
+    foreach (file("style.css") as $t) {
+        $style = $style . $t;
+    }
+    return $style;
+}
 echo("<html lang=ja><head>");
 //MetaData
 echo("<meta charset=\"UTF-8\">");
@@ -7,8 +14,7 @@ echo("<title>Tin☆Program</title>");
 echo("</head><body>");
 //Main
 echo("下のボタンを押すごとにTin☆が増えるよ!<br><button onclick=\"t()\">押してね！</button><br>");
-echo("<div style=\"" . "\">");
-var_dump(file("style.css"));
+echo("<div style=\"" . str_replace(style(),"\n","",style().length) . "\">");
 echo("<script>");
 echo("function t(){");
 echo("document.querySelector(\"div\").innerText+=\"Tin☆\"");
